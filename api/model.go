768575d41c41805953d2302cdbc857d193521f8a
solethus <solethu@ssongca.com>
2024-09-07 15:46:13 +0200

    ### Added - models for request handling

--- api/model.go
+++ api/model.go
@@ -1,1 +1,33 @@
 package api
+
+import "time"
+
+type Events struct {
+	Author    string    `json:"author"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Slideshow struct {
+	Slideshow SlideshowContent `json:"slideshow"`
+}
+
+type SlideshowContent struct {
+	Author string  `json:"author"`
+	Date   string  `json:"date"`
+	Slides []Slide `json:"slides"`
+	Title  string  `json:"title"`
+}
+
+type Slide struct {
+	Title string   `json:"title"`
+	Type  string   `json:"type"`
+	Items []string `json:"items,omitempty"`
+}
+
+type Response struct {
+	MessageID string
+}
+
+type SignupEvent struct {
+	AuthorName string
+}
